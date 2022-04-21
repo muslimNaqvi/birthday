4ebf077799aeeb7441be4abb187742bd2deadb1e
muslimNaqvi <104172847+muslimNaqvi@users.noreply.github.com>
2022-04-21 16:19:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "............."&&b=="...............")
+  	if(a== "Syeda Wila e Fatima"&&b=="Birthday")
   	{
   	    window.location.href="start.html";
   	}
